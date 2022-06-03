6f79d99bae725fe6697d480985065532566df4e6
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-02 20:54:52 -0700

    Comment out my code. Add code from Compass. Both produce same result. But they take FROM cohorts table. Add commented lines for reference tmr during a mentor session.

--- 3_queries_group-by/3_large_cohorts.sql
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -1,6 +1,19 @@
-SELECT cohorts.name as cohort_name, COUNT(students.*) as student_count
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-GROUP BY cohorts.name
-HAVING COUNT(students.*) >= 18
+-- My SQL query:
+-- SELECT cohorts.name as cohort_name, COUNT(students.*) as student_count
+-- FROM students
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- HAVING COUNT(students.*) >= 18
+-- ORDER BY student_count;
+
+-- Compass' SQL query:
+SELECT cohorts.name as cohort_name, count(students.*) AS student_count 
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name 
+HAVING count(students.*) >= 18
 ORDER BY student_count;
+
+-- The lines from SELECT, FROM, to JOIN - What's the diff between
+-- my code and Compass'? Both produce the same result. Which is better
+-- and makes more sense?
