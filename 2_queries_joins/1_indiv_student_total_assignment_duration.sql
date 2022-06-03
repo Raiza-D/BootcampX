39b57a03f0a5ac1236281c97c3707e275b4b1a30
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-02 17:39:00 -0700

    Remove commented notes below code.

--- 2_queries_joins/1_indiv_student_total_assignment_duration.sql
+++ 2_queries_joins/1_indiv_student_total_assignment_duration.sql
@@ -2,14 +2,3 @@ SELECT SUM(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON student_id = students.id
 WHERE students.name = 'Ibrahim Schimmel';
-
-/*
-SUM(assignment_submission.duration) vs. SUM(duration)
-Both will produce the same result.
-By specifying which table duration belongs to, it's more clear at first glance.
-In certain situations (e.g. new junior dev joined the company), having the
-table specified will be more clear. Method 1 has better readability.
-
-To someone who knows the database and its tables well, the table that
-duration belongs to in Method 2 will be obvious.
-*/
