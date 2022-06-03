d16c6c208b6f83e5b5d4987f74b237cf3a1905ca
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-02 21:27:46 -0700

    Add commented lines. Add SQL code from Compass to compare. Both mine and Compass answer produce same results. Speak to mentor about the differences.

--- 3_queries_group-by/5_enrolled_avg_asgmt_completion_time.sql
+++ 3_queries_group-by/5_enrolled_avg_asgmt_completion_time.sql
@@ -1,8 +1,17 @@
--- Currently enrolled students' average assignment completion time:
+-- Currently enrolled students' average assignment completion time
 
+-- My SQL query:
 SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.end_date IS NULL
 GROUP BY students.name
+ORDER BY average_assignment_duration DESC;
+
+-- Compass' SQL query:
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
 ORDER BY average_assignment_duration DESC;
