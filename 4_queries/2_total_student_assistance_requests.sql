cdea4c3eb3aa8881a7cd33409e1c4806998d087d
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-03 15:07:13 -0700

    Create new file. Add query to retrieve total number of assistance requests (ARs) a student has requested.

---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,7 @@
+-- Total number of assistance requests a student has requested
+
+SELECT COUNT(assistance_requests.*) as total_assistances, students.name
+FROM assistance_requests
+JOIN students ON students.id = student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
