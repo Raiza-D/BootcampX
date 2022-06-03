64234d3e781ef1f048de1e59b73589f9c3232121
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-03 16:55:21 -0700

    Add commented line below query for my future reference.

--- 4_queries/5_average_cohort_assistance_time.sql
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -6,3 +6,6 @@ JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
 ORDER BY average_assistance_time ASC;
+
+-- Line 5: Pull data where students.id in students table matching
+-- exactly as student_id data from assistance_requests table.
