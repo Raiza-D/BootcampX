674175219f57b513617eb0433ee1bc7741a689c0
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-03 17:10:47 -0700

    Create new file. Add query to retrieve cohort with the longest average duration of assistance requests.

---
+++ 4_queries/6_cohort_with_longest_average_assistances.sql
@@ -0,0 +1,7 @@
+-- Retrieve cohort with the longest average duration of assistance requests
+
+SELECT cohorts.name, AVG(completed_at - started_at) as average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name;
