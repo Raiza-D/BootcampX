ba9b06caa37b61449791c49675dad93b976b0aae
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-03 18:21:18 -0700

    Edit Line 4 within count parentheses. Used shorthand of asterisk to indicate all records inside assignments table.

--- 4_queries/11_total_assignments_and_duration.sql
+++ 4_queries/11_total_assignments_and_duration.sql
@@ -1,7 +1,7 @@
 -- Retrieve total assignments per day and total duration of assignments
 -- for each day
 
-SELECT day, COUNT(assignments) as number_of_assignments, SUM(duration) as duration
+SELECT day, COUNT(*) as number_of_assignments, SUM(duration) as duration
 FROM assignments
 GROUP BY day
 ORDER BY day;
