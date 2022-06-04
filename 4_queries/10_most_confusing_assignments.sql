a86a9155177f14624af322c544f05671cbaed80b
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-03 18:12:00 -0700

    Edit query to match Compass' solution. Commented out my original query. Will talk to a mentor to disucss both.

--- 4_queries/10_most_confusing_assignments.sql
+++ 4_queries/10_most_confusing_assignments.sql
@@ -1,7 +1,15 @@
 -- Retrieve assignments and their total assistance requests
 
-SELECT assignments.id, assignments.name, assignments.day, assignments.chapter, COUNT(assistance_requests.*) as total_requests
-FROM assistance_requests
-JOIN assignments ON assignments.id = assignment_id
+SELECT assignments.id, name, day, chapter, COUNT(assistance_requests) as total_requests
+FROM assignments
+JOIN assistance_requests ON assignments.id = assignment_id
 GROUP BY assignments.id
 ORDER BY total_requests DESC;
+
+
+-- My original query:
+-- SELECT assignments.id, assignments.name, assignments.day, assignments.chapter, COUNT(assistance_requests.*) as total_requests
+-- FROM assistance_requests
+-- JOIN assignments ON assignments.id = assignment_id
+-- GROUP BY assignments.id
+-- ORDER BY total_requests DESC;
