121774708245daf0cba87b5187bba4dc1d3f8aad
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-03 18:04:59 -0700

    Create new file. Add query to retrieve total assistance requests for each assignment.

---
+++ 4_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,7 @@
+-- Retrieve assignments and their total assistance requests
+
+SELECT assignments.id, assignments.name, assignments.day, assignments.chapter, COUNT(assistance_requests.*) as total_requests
+FROM assistance_requests
+JOIN assignments ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
