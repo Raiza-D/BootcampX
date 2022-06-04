9523f9756761fb81f0fb2193de130a2de3926db7
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-03 17:52:27 -0700

    Edit code to match Compass. My prior solution produced the same result.

--- 4_queries/9_cohort_average_assistance_duration.sql
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -1,11 +1,13 @@
 -- Retrieve average total duration of assistance requests for each cohort
 -- Return a single row called 'average_total_duration'
-SELECT AVG(totals.total_duration) as average_total_duration
+
+SELECT AVG(total_duration) as average_total_duration
 
 FROM (
-  SELECT SUM(completed_at - started_at) as total_duration
+  SELECT cohorts.name as cohort, SUM(completed_at - started_at) as total_duration
   FROM assistance_requests
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
   GROUP BY cohorts.name
-) as totals;
+  ORDER BY total_duration
+) as total_durations;
